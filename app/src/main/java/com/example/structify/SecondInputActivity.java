b44eb17ddaa92ceba379ae8c805ab593daf4b264
Adrian Lee <adrianlee0118@gmail.com>
2019-12-10 13:11:35 -0800

    Building dynamic GUI and logic at the same time. xml file for Second activity added.
    Google API only allows reading and writing data. Need code for a calendar display that provides a summary of study times that is preferably filterable.

--- app/src/main/java/com/example/structify/SecondInputActivity.java
+++ app/src/main/java/com/example/structify/SecondInputActivity.java
@@ -6,6 +6,7 @@ import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,15 +14,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.widget.NestedScrollView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import static android.graphics.Typeface.BOLD;
 
 //The second page, where, given the number of courses, course data is input. The button will store
-//all values from the editText fields. Since the editTexts are generated dynamically via java and thus can only
-//have integer ids, use a map to store variable names as strings, mapped to the int ids.
+//all values from the editText fields. Steps:
+//1) Dynamically generate all input fields for courses using a for loop based on NumCourses.
+//        -EditTexts are stored in an ArrayList so that they persist
+//        -EditTexts have string IDs attached using a Map
+//2) Receive user input through GUI
+//3) Activity takes data from input fields and puts them into objects of UniversityCourse class.
 
 public class SecondInputActivity extends AppCompatActivity {
 
@@ -31,10 +39,23 @@ public class SecondInputActivity extends AppCompatActivity {
     private int NumCourses;
     private int StudyTime;
 
-    //A Map for referencing EditText data by name, to be instantiated as fields are created
-    //An array to store the new UniversityCourse objects.
-    private Map<String,EditText> Data;
-    private ArrayList<UniversityCourse> Semester;
+    //Making dynamically created EditText fields readable.
+    private Map<String,EditText> InputFieldIDs;
+    private List<LinearLayout> Panels;  //not accessed, just to allow layouts to persist outside loops
+    private List<HorizontalScrollView> WtPanels;  //not accessed, just allowing layouts to persist
+    private List<TextView> Labels;      //not accessed, just to allow labels to persist outside loop
+    private List<EditText> InputFields;
+
+    //All Course Data stored according to index (e.g. i=0 to i=7)
+    private ArrayList<String> CourseNames;
+    private ArrayList<Integer> CourseWeights;
+    private ArrayList<Integer> FinalWeights;
+    private ArrayList<Date> FinalDates;
+    private ArrayList<Integer> MTWeights;
+    private ArrayList<ArrayList<Date>> MTDates;
+    private ArrayList<Integer> AssignWts;
+    private ArrayList<ArrayList<Date>> AssignDates;
+
 
     //The layout where we will put all of the dynamic UI-generated fields
     private NestedScrollView Canvas;
@@ -44,6 +65,7 @@ public class SecondInputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondinput);
 
+        //Grabbing data from MainActivity
         Bundle extras = getIntent().getExtras();
         StartDate = new Date();
         StartDate.setTime(extras.getLong("StartDate"));
@@ -52,6 +74,7 @@ public class SecondInputActivity extends AppCompatActivity {
         NumCourses = extras.getInt("NumCourses");
         StudyTime = extras.getInt("StudyTime");
 
+        //Setting our base
         Canvas = findViewById(R.id.canvas);
 
         //Standard formatting for base linearLayout, other components' formats are defined in styles.xml
@@ -68,23 +91,72 @@ public class SecondInputActivity extends AppCompatActivity {
 
             //First level: Course name info
             LinearLayout linearLayout0 = new LinearLayout(new ContextThemeWrapper(this, R.style.layout_rows_style),null,0);
+            Panels.add(linearLayout0);
             TextView c = new TextView(new ContextThemeWrapper(this,R.style.textview_style),null,0);
+            Labels.add(c);
             c.setText("Course " + Integer.toString(i)+ " Name");
             linearLayout0.addView(c);
             EditText cc = new EditText(new ContextThemeWrapper(this,R.style.edittext_style),null,0);
-            cc.setHint("Enter course name");
+            InputFields.add(cc);
+            cc.setHint("Enter course "+Integer.toString(i)+" name");
             linearLayout0.addView(cc);
             //Store the ID!
             String tname = "Course" + Integer.toString(i)+"Name";
-            Data.put(tname, cc);
+            InputFieldIDs.put(tname, cc);
             linearLayoutbase.addView(linearLayout0);
 
-            //Second Level: Final Wt, Midterm Wt, Assignments/Quiz Wts
-            LinearLayout linearLayout1 = new LinearLayout(new ContextThemeWrapper(this, R.style.layout_rows_style),null,0);
+            //Second Level has 3 items in a side scroller: Final Wt, Midterm Wt, Assignments/Quiz Wts
+            HorizontalScrollView linearLayout1 = new HorizontalScrollView(new ContextThemeWrapper(this, R.style.layout_rows_style),null,0);
+            WtPanels.add(linearLayout1);
             TextView fwt = new TextView(new ContextThemeWrapper(this,R.style.textview_style),null,0);
-            fwt.setText("Final 1 Weight");
-            linearLayout0.addView(fwt);
+            Labels.add(fwt);
+            fwt.setText("Final"+Integer.toString(i)+"Weight");
+            linearLayout1.addView(fwt);
             EditText fwtt = new EditText(new ContextThemeWrapper(this,R.style.edittext_style),null,0);
+            InputFields.add(fwtt);
+            fwtt.setHint("Enter percentage");
+            linearLayout1.addView(fwtt);
+            tname = "Course"+Integer.toString(i)+"FinalWt";
+            InputFieldIDs.put(tname,fwtt);
+            TextView mtwt = new TextView(new ContextThemeWrapper(this,R.style.textview_style),null,0);
+            Labels.add(mtwt);
+            mtwt.setText("Midterm"+Integer.toString(i)+"Weight");
+            linearLayout1.addView(mtwt);
+            EditText mtwtt = new EditText(new ContextThemeWrapper(this,R.style.edittext_style),null,0);
+            InputFields.add(mtwtt);
+            mtwtt.setHint("Enter percentage");
+            linearLayout1.addView(mtwtt);
+            tname = "Course"+Integer.toString(i)+"MidtermWt";
+            InputFieldIDs.put(tname,mtwtt);
+            TextView aswt = new TextView(new ContextThemeWrapper(this,R.style.textview_style),null,0);
+            Labels.add(aswt);
+            aswt.setText("AssignmentandQuiz"+Integer.toString(i)+"Weight");
+            linearLayout1.addView(aswt);
+            EditText aswtt = new EditText(new ContextThemeWrapper(this,R.style.edittext_style),null,0);
+            InputFields.add(aswtt);
+            aswtt.setHint("Enter percentage");
+            linearLayout1.addView(aswtt);
+            tname = "Course"+Integer.toString(i)+"AssignmentWt";
+            InputFieldIDs.put(tname,aswtt);
+            linearLayoutbase.addView(linearLayout1);
+
+            //3rd level: Final Exam Date
+            LinearLayout linearLayout2 = new LinearLayout(new ContextThemeWrapper(this, R.style.layout_rows_style),null,0);
+            Panels.add(linearLayout2);
+            TextView fd = new TextView(new ContextThemeWrapper(this,R.style.textview_style),null,0);
+            Labels.add(fd);
+            fd.setText("Final " + Integer.toString(i)+ " Date");
+            linearLayout2.addView(fd);
+            EditText fdd = new EditText(new ContextThemeWrapper(this,R.style.edittext_style),null,0);
+            InputFields.add(fdd);
+            fdd.setHint("YYYY-MM-DD");
+            linearLayout2.addView(fdd);
+            tname = "Final" + Integer.toString(i)+"Date";
+            InputFieldIDs.put(tname, fdd);
+            linearLayoutbase.addView(linearLayout2);
+
+
+            linearLayoutbase.addView(linearLayout2);
 
         }
 
